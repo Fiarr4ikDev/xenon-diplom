f025b9568b4efef459295a4b15f765b4abc8c2f1
Fiarr4ikDev <fiarr4ikdev@gmail.com>
2025-05-02 19:07:05 +0700

    init commit

---
+++ supplier-service/src/main/java/ru/fiarr4ik/supplierservice/SupplierServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.fiarr4ik.supplierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SupplierServiceApplication.class, args);
+    }
+}
