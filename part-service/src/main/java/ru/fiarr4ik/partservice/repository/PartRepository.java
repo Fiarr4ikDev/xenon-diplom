167477ab8187322c38088481fe19e0e5df3f5a9a
Fiarr4ik <fiarr4ik@gmail.com>
2025-05-14 13:25:40 +0700

    feat: работа над part-service
    - сделал репозиторий для сущности Part c методом findPartByPartId

---
+++ part-service/src/main/java/ru/fiarr4ik/partservice/repository/PartRepository.java
@@ -0,0 +1,12 @@
+package ru.fiarr4ik.partservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.fiarr4ik.partservice.entity.Part;
+
+import java.util.Optional;
+
+public interface PartRepository extends JpaRepository<Part, Long> {
+
+    Optional<Part> findPartByPartId(Long partId);
+
+}
