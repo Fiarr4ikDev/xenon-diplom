b42f55525ae60bd9ce7f3b8bbcc9ec173512f5c5
Fiarr4ik <fiarr4ik@gmail.com>
2025-05-14 12:20:33 +0700

    feat: добавил пустой part-service

---
+++ part-service/src/test/java/ru/fiarr4ik/partservice/PartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.partservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
