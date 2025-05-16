7004d41618f417bd65e54999e90c918fc95ddb5d
Fiarr4ik <fiarr4ik@gmail.com>
2025-05-16 19:40:24 +0700

    работа над всем проектом
    - feat: перенёс весь проект в Eureka
    - fix: исправил везде pom файлы
    - feat: обновил .gitignore

--- supplier-service/src/main/java/ru/fiarr4ik/supplierservice/SupplierServiceApplication.java
+++ supplier-service/src/main/java/ru/fiarr4ik/supplierservice/SupplierServiceApplication.java
@@ -2,9 +2,11 @@ package ru.fiarr4ik.supplierservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SupplierServiceApplication {
 
     public static void main(String[] args) {
