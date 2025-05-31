f488de46e2969746a08cce15f41073a11cd2850d
Fiarr4ik <fiarr4ik@gmail.com>
2025-05-31 15:15:58 +0700

    работа security-service
    -fix

--- security-service/src/main/java/ru/fiarr4ik/securityservice/config/SecurityConfig.java
+++ security-service/src/main/java/ru/fiarr4ik/securityservice/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package ru.fiarr4ik.securityservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,22 +10,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.fiarr4ik.securityservice.service.UserService;
+import ru.fiarr4ik.securityservice.repository.UserRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
 
-    public SecurityConfig(@Lazy UserService userService) {
-        this.userService = userService;
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> userService.findByUsername(username)
+        return username -> userRepository.findByUsername(username)
                 .map(user -> org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
                         .password(user.getPassword())
                         .authorities("USER")
